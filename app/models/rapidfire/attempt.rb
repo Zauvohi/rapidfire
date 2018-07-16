37f2065cc2081f6d10115307cfd4c1418f38f6e3
Alejandro Prado <alejandro.prado@densitylabs.io>
2018-07-16 17:54:34 -0500

    add about user to attempts

--- app/models/rapidfire/attempt.rb
+++ app/models/rapidfire/attempt.rb
@@ -1,8 +1,9 @@
 module Rapidfire
   class Attempt < ActiveRecord::Base
-    belongs_to :survey
+    belongs_to :survey, inverse_of: :attempts
     belongs_to :user, polymorphic: true
     has_many   :answers, inverse_of: :attempt, autosave: true
+    belongs_to :about_user, foreign_key: 'about_user_id', class_name: 'User'
 
     if Rails::VERSION::MAJOR == 3
       attr_accessible :survey, :user
